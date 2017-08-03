127aa007787c4bdc49b7576786903dba818288ff
Simon Curty <simon.curty@students.unibe.ch>
2017-08-03 16:38:34 +0200

    revised configuration file loading

--- src/main/java/ch/unibe/scg/curtys/bugreportquality/configuration/ConfigurationLoader.java
+++ src/main/java/ch/unibe/scg/curtys/bugreportquality/configuration/ConfigurationLoader.java
@@ -3,8 +3,8 @@ package ch.unibe.scg.curtys.bugreportquality.configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author curtys
@@ -13,9 +13,8 @@ public class ConfigurationLoader {
 
 	public static Configuration load(String configFilePath) throws IOException {
 		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-		ClassLoader classLoader = ConfigurationLoader.class.getClassLoader();
-		File yamlFile = new File(classLoader.getResource(configFilePath).getFile());
-		Configuration config = mapper.readValue(yamlFile, Configuration.class);
+		InputStream in = ConfigurationLoader.class.getResourceAsStream(configFilePath);
+		Configuration config = mapper.readValue(in, Configuration.class);
 		return config;
 	}
 }
